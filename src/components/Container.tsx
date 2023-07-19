af66737617f8b33c2eda10ab74eea485c05359ca
santidotpy <santidotpy@gmail.com>
2023-07-19 03:44:20 -0300

    add utils

---
+++ src/components/Container.tsx
@@ -0,0 +1,9 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container = ({ children }: ContainerProps) => {
+  return <div className="container mx-auto px-4">{children}</div>;
+};
+
+export default Container;
---
+++ src/components/No-result.tsx
@@ -0,0 +1,16 @@
+export default function NoResults() {
+  return (
+    // <div className="flex flex-col items-center justify-center">
+    // <h1 className="text-3xl font-bold text-center text-[#fff]">
+    //     No results found
+    // </h1>
+    // <h1 className="text-3xl font-bold text-center text-[#fff]">
+    //     Please try again
+    // </h1>
+    // </div>
+
+    <div className="flex items-center justify-center h-full w-full text-neutral-500">
+      No results found.
+    </div>
+  );
+}
