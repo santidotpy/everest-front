a6327c4088ac462caed78f1d0ad87eb1e741fa25
santidotpy <santidotpy@gmail.com>
2023-07-19 03:46:40 -0300

    🔧 update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        LOGIN_ROUTE:process.env.LOGIN_ROUTE,
-    }
+  env: {
+    LOGIN_ROUTE: process.env.LOGIN_ROUTE,
+  },
 
-}
+  images: {
+    domains: [
+      "loremflickr.com",
+      "picsum.photos",
+      "nikearprod.vtexassets.com",
+      "static.nike.com",
+      "static.wikia.nocookie.net",
+      "assets.adidas.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
