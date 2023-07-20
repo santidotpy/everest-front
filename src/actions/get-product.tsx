5d057151609116150a38e88628b1e6bc55259fc3
santidotpy <santidotpy@gmail.com>
2023-07-20 01:01:03 -0300

    ✨ add get single product request

---
+++ src/actions/get-product.tsx
@@ -0,0 +1,27 @@
+import qs from "query-string";
+
+import { Product, Docs } from "../../types";
+
+const URL = `${process.env.BACKEND_URL}/api/product`;
+
+interface Query {
+  id?: string;
+}
+
+const getProduct = async (query: Query): Promise<Product[]> => {
+  const url = qs.stringifyUrl({
+    url: URL,
+    query: {
+      id: query.id,
+    },
+  });
+
+  const res = await fetch(
+    url
+    // { next: { revalidate: 10 } }
+  );
+  const data = await res.json();
+  return data;
+};
+
+export default getProduct;
